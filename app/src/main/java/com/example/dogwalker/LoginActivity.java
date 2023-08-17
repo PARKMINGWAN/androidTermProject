fb4d685dcabb4fab029bc100c360ce3659104ef7
PARKMINGWAN <61464552+PARKMINGWAN@users.noreply.github.com>
2023-08-17 12:26:43 +0900

    로그인 화면 날라간거 복구 2023-08-17-12:26박민관

--- app/src/main/java/com/example/dogwalker/LoginActivity.java
+++ app/src/main/java/com/example/dogwalker/LoginActivity.java
@@ -19,6 +19,7 @@ import android.widget.Toast;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+
 import com.google.android.gms.auth.api.identity.BeginSignInRequest;
 import com.google.android.gms.auth.api.identity.Identity;
 import com.google.android.gms.auth.api.identity.SignInClient;
@@ -46,39 +47,11 @@ public class LoginActivity extends AppCompatActivity {
     private Button btnCancel,btnLogin;
 
 
-/* 수정중
     private SignInClient oneTapClient;
     private BeginSignInRequest signInRequest;
     private static final int REQ_ONE_TAP = 2;
     private boolean showOneTapUI = true;
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
 
-        switch (requestCode) {
-            case REQ_ONE_TAP:
-                try {
-                    // ...
-                } catch (ApiException e) {
-                    switch (e.getStatusCode()) {
-                        case CommonStatusCodes.CANCELED:
-                            Log.d("TAG", "One-tap dialog was closed.");
-                            // Don't re-prompt the user.
-                            showOneTapUI = false;
-                            break;
-                        case CommonStatusCodes.NETWORK_ERROR:
-                            Log.d("TAG", "One-tap encountered a network error.");
-                            // Try again or just ignore.
-                            break;
-                        default:
-                            Log.d("TAG", "Couldn't get credential from result."
-                                    + e.getLocalizedMessage());
-                            break;
-                    }
-                }
-                break;
-        }
-    }
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState,
                          @Nullable PersistableBundle persistentState) {
@@ -99,43 +72,9 @@ public class LoginActivity extends AppCompatActivity {
                 // Automatically sign in when exactly one credential is retrieved.
                 .setAutoSelectEnabled(true)
                 .build();
-        BeginSignInRequest  signInRequest = BeginSignInRequest.builder()
-                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                        .setSupported(true)
-                        // Your server's client ID, not your Android client ID.
-                        .setServerClientId(getString(R.string.default_web_client_id))
-                        // Only show accounts previously used to sign in.
-                        .setFilterByAuthorizedAccounts(true)
-                        .build())
-                .build();
+        // ...
+    }
 
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        SignInCredential googleCredential = oneTapClient.getSignInCredentialFromIntent(intent);
-        String idToken = googleCredential.getGoogleIdToken();
-        if (idToken !=  null) {
-            // Got an ID token from Google. Use it to authenticate
-            // with Firebase.
-            AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
-            mAuth.signInWithCredential(firebaseCredential)
-                    .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                Log.d("login success", "signInWithCredential:success");
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                startActivity(intent);
-
-                                finish();
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.w("google Login fail", "signInWithCredential:failure", task.getException());
-                            }
-                        }
-                    });
-        }
-    }*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
