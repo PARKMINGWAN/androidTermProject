c56004a811d07dacf8a77166494f5345e1f46c40
jiniee99 <kdh8616@naver.com>
2023-08-21 13:15:36 +0900

    임지연 2023-08-21 13:14 tab2에 map 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,10 @@ dependencies {
     implementation ("com.firebaseui:firebase-ui-auth:7.2.0")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.android.gms:play-services-auth:20.6.0")
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.naver.maps:map-sdk:3.17.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
