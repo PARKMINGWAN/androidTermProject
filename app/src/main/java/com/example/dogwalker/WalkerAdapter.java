cc217310a67873bc1997f41fb3c13c89bd0d2c81
jungyerang <132261862+jungyerang@users.noreply.github.com>
2023-08-21 11:41:37 +0900

    정예랑 2023-08-21 11:41 firebase 데이터베이스 저장(2)

--- app/src/main/java/com/example/dogwalker/WalkerAdapter.java
+++ app/src/main/java/com/example/dogwalker/WalkerAdapter.java
@@ -2,6 +2,7 @@ package com.example.dogwalker;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +17,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -32,17 +35,18 @@ public class WalkerAdapter
     private OnItemClickListener onItemClickListener;
 
    public void addItem(Walker walker){
-
        mDatabase = FirebaseDatabase.getInstance().getReference("users");
 
-       mDatabase.child("Id").setValue(walker.getId().toString());
-
-       mDatabase.child("Id").child("Name").setValue(walker.getName().toString());
-       mDatabase.child("Id").child("PassWord").setValue(walker.getPwd().toString());
-       mDatabase.child("Id").child("Tel").setValue(walker.getTel().toString());
-       mDatabase.child("Id").child("Address").setValue(walker.getAddr().toString());
-       mDatabase.child("Id").child("Career").setValue(walker.getCareer().toString());
-       mDatabase.child("Id").child("Nurture").setValue(walker.getNurture().toString());
+       FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+       String uid = user.getUid();
+       Log.d("getUid",uid);
+       mDatabase.child(uid).child("userId").setValue(walker.getId().toString());
+       mDatabase.child(uid).child("Name").setValue(walker.getName().toString());
+       mDatabase.child(uid).child("PassWord").setValue(walker.getPwd().toString());
+       mDatabase.child(uid).child("Tel").setValue(walker.getTel().toString());
+       mDatabase.child(uid).child("Address").setValue(walker.getAddr().toString());
+       mDatabase.child(uid).child("Career").setValue(walker.getCareer().toString());
+       mDatabase.child(uid).child("Nurture").setValue(walker.getNurture().toString());
 
 /*
        mDatabase.child(userId).setValue(walker.getName().toString());
